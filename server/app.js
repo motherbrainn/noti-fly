a991928ff38195398879a883f45b4c4d4dc330a9
FSpanthor <37132756+FSpanthor@users.noreply.github.com>
2022-06-23 09:48:01 -0700

    add some logic for start and stop using fake db

--- server/app.js
+++ server/app.js
@@ -6,7 +6,11 @@ dotenv.config();
 const PORT = process.env.PORT || 4000;
 const env = process.env.NODE_ENV;
 
+const MessagingResponse = require("twilio").twiml.MessagingResponse;
+
 const app = express();
+
+let fakeDb = [];
 
 //set cors here to allow requests from client
 app.use(
@@ -32,13 +36,65 @@ app.post("/sendMessage", (req, res) => {
   const client = new twilio(accountSid, authToken);
   client.messages
     .create({
-      body: "Hello from Node",
+      body: "Hello from Node. Reply with 'stop' to unsubscribe",
       to: process.env.TEST_PHONE_NUMBER, // Text this number
       from: process.env.TWILIO_PHONE_NUMBER, // From a valid Twilio number
     })
     .then((message) => console.log(message.sid));
 
   res.send({ res: "send message" });
+});
+
+const stopKeyWords = [
+  "STOP",
+  "STOPALL",
+  "UNSUBSCRIBE",
+  "CANCEL",
+  "END",
+  "QUIT",
+];
+const startKeyWords = ["START", "YES", "UNSTOP"];
+
+app.post("/sms", (req, res) => {
+  const messageBody = req.body.Body.toUpperCase();
+  const fromPhoneNumber = req.body.From;
+
+  //if this is a brand new user, add user to db
+  if (fakeDb.findIndex((entry) => entry.phoneNumber === fromPhoneNumber)) {
+    console.log("adding new number");
+    fakeDb.push({ phoneNumber: fromPhoneNumber, subscribed: true });
+  }
+
+  //check if we need to unsubscribe user
+  if (stopKeyWords.findIndex((word) => word === messageBody) !== -1) {
+    console.log("add user to stop list");
+
+    fakeDb = fakeDb.map((entry) => {
+      if (entry.phoneNumber === fromPhoneNumber) {
+        return { ...entry, subscribed: false };
+      }
+    });
+
+    console.log({ fakeDb });
+  }
+
+  //check if we need to subscribe user
+  if (startKeyWords.findIndex((word) => word === messageBody) !== -1) {
+    console.log("add user to start list");
+    fakeDb = fakeDb.map((entry) => {
+      if (entry.phoneNumber === fromPhoneNumber) {
+        return { ...entry, subscribed: true };
+      }
+    });
+    console.log({ fakeDb });
+  }
+
+  const twiml = new MessagingResponse();
+
+  twiml.message("hello");
+
+  res.writeHead(200, { "Content-Type": "text/xml" });
+  res.end(twiml.toString());
 });
 
 app.listen(PORT, (error) => {
