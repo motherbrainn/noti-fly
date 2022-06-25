b59ebbfca39ba124e4b442f25c218266ea660ec0
FSpanthor <37132756+FSpanthor@users.noreply.github.com>
2022-06-24 23:27:35 -0700

    remove sendMessage route in server

--- server/app.js
+++ server/app.js
@@ -36,24 +36,6 @@ app.use(
 );
 app.use(router);
 
-// POST method route
-app.post("/sendMessage", (req, res) => {
-  const accountSid = process.env.TWILIO_ACCOUNT_SID; // Your Account SID from www.twilio.com/console
-  const authToken = process.env.TWILIO_AUTH_TOKEN; // Your Auth Token from www.twilio.com/console
-
-  const twilio = require("twilio");
-  const client = new twilio(accountSid, authToken);
-  client.messages
-    .create({
-      body: "Hello from Node. Reply with 'stop' to unsubscribe",
-      to: process.env.TEST_PHONE_NUMBER, // Text this number
-      from: process.env.TWILIO_PHONE_NUMBER, // From a valid Twilio number
-    })
-    .then((message) => console.log(message.sid));
-
-  res.send({ res: "send message" });
-});
-
 app.listen(PORT, (error) => {
   if (!error) console.log("App listening on port: " + PORT);
   else console.log("Error occurred: ", error);
