ca1b3051b478b387bed8cace103404cf3afb673f
FSpanthor <37132756+FSpanthor@users.noreply.github.com>
2022-07-02 23:57:23 -0700

    add logging to sendConfirmation

--- server/utilities.js
+++ server/utilities.js
@@ -7,6 +7,8 @@ const sendTextMessage = async (phoneNumber, messageContent) => {
   const accountSid = process.env.TWILIO_ACCOUNT_SID; // Your Account SID from www.twilio.com/console
   const authToken = process.env.TWILIO_AUTH_TOKEN; // Your Auth Token from www.twilio.com/console
 
+  console.log(accountSid);
+  console.log(authToken);
   const client = new twilio(accountSid, authToken);
   const response = client.messages
     .create({
