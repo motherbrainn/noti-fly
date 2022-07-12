2e4bca4ef7ef4ee3d5c44be28dfeb9a5ea420809
FSpanthor <37132756+FSpanthor@users.noreply.github.com>
2022-07-11 22:09:41 -0700

    style notification-sent

--- client/pages/notification-sent.tsx
+++ client/pages/notification-sent.tsx
@@ -1,4 +1,20 @@
+import { Dialog, DialogTitle } from "@mui/material";
+import styles from "../styles/Home.module.css";
+
 const NotificationSent = () => {
-  return <div>notification successfully sent</div>;
+  return (
+    <div className={styles.container}>
+      <Dialog
+        open={true}
+        aria-labelledby="alert-dialog-title"
+        aria-describedby="alert-dialog-description"
+        PaperProps={{ sx: { backgroundColor: "aliceblue" } }}
+      >
+        <DialogTitle id="alert-dialog-title">
+          {"Notification successfully sent"}
+        </DialogTitle>
+      </Dialog>
+    </div>
+  );
 };
 export default NotificationSent;
