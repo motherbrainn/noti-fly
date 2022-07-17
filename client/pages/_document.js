9939c71189f564c015782d28309b59eaa4b3cac2
FSpanthor <37132756+FSpanthor@users.noreply.github.com>
2022-07-16 23:16:24 -0700

    update font

--- client/pages/_document.js
+++ client/pages/_document.js
@@ -7,6 +7,8 @@ class MyDocument extends Document {
     return (
       <Html>
         <Head>
+          <link rel="preconnect" href="https://fonts.googleapis.com" />
+          <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
           <link
             href="https://fonts.googleapis.com/css2?family=Press+Start+2P&display=swap"
             rel="stylesheet"
